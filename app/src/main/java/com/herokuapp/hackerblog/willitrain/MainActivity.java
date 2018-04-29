cf2180b8151cab84078cf84402794598ecfbdc5f
tyler-furby <tyler@tylerfurby.com>
2018-04-28 19:17:44 -0600

    Add buggy API call.

--- app/src/main/java/com/herokuapp/hackerblog/willitrain/MainActivity.java
+++ app/src/main/java/com/herokuapp/hackerblog/willitrain/MainActivity.java
@@ -1,13 +1,84 @@
 package com.herokuapp.hackerblog.willitrain;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    JSONObject data = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getJSON("Denver");
     }
+
+    public void getJSON(final String city) {
+        // TODO: Fill out AsyncTask
+        new AsyncTask<Void, Void, Void>() {
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+            }
+
+            @Override
+            // TODO: Fill out doInBackground
+            protected Void doInBackground(Void... params) {
+                try {
+                    URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=222aa7d3131cb8f944331a250feeb578");
+
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                    StringBuffer json = new StringBuffer(1024);
+                    String tmp = "";
+
+                    while((tmp = reader.readLine()) != null) {
+                        json.append(tmp).append("\n");
+                    }
+                    reader.close();
+
+                    data = new JSONObject(json.toString());
+
+                    if(data.getInt("cod") != 200) {
+                        System.out.println("Error...");
+                        return null;
+                    }
+
+
+                } catch (Exception e) {
+
+                    System.out.println("Exception "+ e.getMessage());
+                    return null;
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void Void) {
+                if(data != null){
+                    Log.d("Weather", data.toString());
+                }
+
+            }
+        }.execute();
+
+    }
+
+
 }
