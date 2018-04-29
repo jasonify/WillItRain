6000457dfd788d259e8d8463ff8544900b663de0
jason <bjason@gmail.com>
2018-04-28 23:29:31 -0700

    Get another prediction

--- app/src/main/java/com/herokuapp/hackerblog/willitrain/MainActivity.java
+++ app/src/main/java/com/herokuapp/hackerblog/willitrain/MainActivity.java
@@ -14,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +30,66 @@ public class MainActivity extends AppCompatActivity {
         upcomingWeatherTV = (TextView) findViewById(R.id.upcomingWeather);
 
         getJSON("Denver");
+        getPrediction("Denver");
+
+    }
+
+    public void getPrediction(final String city) {
+        // TODO: Fill out AsyncTask
+        new AsyncTask<Void, Void, Void>() {
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+            }
+
+            @Override
+            // TODO: Fill out doInBackground
+            protected Void doInBackground(Void... params) {
+                try {
+                    URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?q="+city+"&APPID=222aa7d3131cb8f944331a250feeb578");
+                    System.out.println(url.toString());
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                    StringBuffer json = new StringBuffer(1024);
+                    String tmp = "";
+
+                    while((tmp = reader.readLine()) != null) {
+                        json.append(tmp).append("\n");
+                    }
+                    reader.close();
+
+                    JSONObject data2 = new JSONObject(json.toString());
+
+                    ArrayList<Weather> list = Weather.fromJSONArray(data2.getJSONArray("list"));
+
+
+                    if(data2.getInt("cod") != 200) {
+                        System.out.println("===========Error...");
+                        return null;
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("Exception !!!!!!");
+                    System.out.println("Exception "+ e.getMessage());
+                    return null;
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void Void) {
+                if(data != null){
+                    Log.d("Weather", data.toString());
+                }
+            }
+        }.execute();
+
     }
 
     public void getJSON(final String city) {
